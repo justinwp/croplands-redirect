2ae3a153fc84c62a219461d600ead0d5d59cd4d6
Justin Poehnelt <justin.poehnelt@gmail.com>
2015-07-01 00:49:10 -0500

    logging on request

--- web.js
+++ web.js
@@ -3,6 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('*', function(request, response) {
+  console.log(JSON.stringify(request));
   response.redirect(process.env.NEW_BASE_URL + request.url)
 });
 
